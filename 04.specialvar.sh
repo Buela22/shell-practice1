63128d98414f81eed890b7e4d03c4b3eb8d636ca
Buela <chbuela22@gmail.com>
2025-11-13 21:28:01 +0530

    Done

--- 04.specialvar.sh
+++ 04.specialvar.sh
@@ -2,3 +2,8 @@
 
 echo "All varaibles passed to the script : $@"
 echo "All varaibles passed to the script : $*"
+echo "Script Name : $0"
+echo "current directory : $PWD"
+echo "Who is running the script : $USER"
+echo "home Directory :$HOME"
+echo "Process id : $$"
