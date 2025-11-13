af6cf3540e3cdb4398609b905ef3d3414daa18b4
Buela <chbuela22@gmail.com>
2025-11-13 21:48:59 +0530

    okay

--- 06.conditions.sh
+++ 06.conditions.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "False the $NUMBER is less than 6 "
+elif [ $NUMBER -eq 10 ]; then
+    echo "The $NUMBER is equal "    
 else
      echo "True the $NUMBER is greater than 6"
 fi     
