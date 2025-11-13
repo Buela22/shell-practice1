1df46c7f48433c4d91d1fe37ce7d531822512df7
Buela <chbuela22@gmail.com>
2025-11-13 20:56:18 +0530

    Varaibles

---
+++ 02varaibles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VAR_NAME=value
+$VAR_NAME
+
+
+$1="Buela"
+$2="Buddy"
+echo "$1": Heyy hai "$2" how are u
+echo "$2": Hi "$1" i am good 
+echo "$1": happie to meet u "$2"
+echo "$2": Great same here "$1"
