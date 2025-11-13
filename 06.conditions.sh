7e00577ba5b729dcf0ac136457516bd0e2558631
Buela <chbuela22@gmail.com>
2025-11-13 21:46:36 +0530

    okay

--- 06.conditions.sh
+++ 06.conditions.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-NUMBER=10
-if [ $N -lt 6 ]; then
+NUMBER=$1
+
+if [ $NUMBER -lt 10 ]; then
     echo "False the number is less than 6 "
 else
      echo "True the number is greater than 6"
 fi     
+
+
+
